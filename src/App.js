01cf1fdb2e802390bbcde3d28aacbdb886474a65
vladislav <vl_evlashin@mail.ru>
2022-12-17 12:37:26 +0300

    Add minimal logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.scss';
 import Header from "./components/Header/Header";
+import RunningString from './components/RunningString/RunningString';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className='Main-part'>
+        <RunningString />
+      </main>
+      
     </div>
   );
 }
--- src/components/RunningString/RunningString.jsx
+++ src/components/RunningString/RunningString.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React from "react"
+import './RunningString.scss'
+import {useState, useEffect} from 'react'
 
 const RunningString = () => {
+
+    const [startWord, setStartWord] = useState('йцуке фывап ячсм');
+    const [endWord, setEndWord] = useState('')
+
+    const CurrectInput = () => {
+
+        console.log(startWord[1])
+        if (startWord.length < 1) {
+        } else {
+            startWord.substring(1)
+            setStartWord(startWord => startWord.substring(1))
+          setEndWord(word => word +startWord[0])
+    
+        }
+        console.log('pressed Enter ✅');
+      };
+      useEffect(() => {
+        const keyDownHandler = event => {
+          console.log('User pressed: ', event.key);
+    
+          if (event.key === startWord[0]) {
+            event.preventDefault();
+    
+            // 👇️ your logic here
+            CurrectInput();
+          } else {
+          }
+        };
+    
+        document.addEventListener('keydown', keyDownHandler);
+    
+        return () => {
+          document.removeEventListener('keydown', keyDownHandler);
+        };
+      }, [startWord]);
     return (
-        <div className="running-string"></div>
+        <div className="running-string">
+            <div className="end-string">{endWord}</div>
+            <div className="start-string">{startWord}</div>
+        </div>
     )
 }
 
