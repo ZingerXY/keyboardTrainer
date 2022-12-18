0e7d6b1050a40460345ba8ffe5d0c44e65dd0fa2
AnastasiyaVi <nastyonaki@mail.ru>
2022-12-18 12:59:02 +0300

    add basic style for homepage (demo)

--- src/components/RunningString/RunningString.jsx
+++ src/components/RunningString/RunningString.jsx
@@ -1,67 +1,71 @@
 import React from "react"
 import './RunningString.scss'
-import {useState, useEffect, createRef} from 'react'
+import { useState, useEffect, createRef } from 'react'
 
 const RunningString = () => {
-    const [startWord, setStartWord] = useState('йцуке фывап ячсм'); //Слово, которое надо набрать
-    const [endWord, setEndWord] = useState('') // Набранное слово
-    const stringId = createRef() // Генерирует Ref для строки (Понадобиться для добавления стилей при неправильном вводе)
+  const [startWord, setStartWord] = useState('йцуке фывап ячсм'); //Слово, которое надо набрать
+  const [endWord, setEndWord] = useState('') // Набранное слово
+  const stringId = createRef() // Генерирует Ref для строки (Понадобиться для добавления стилей при неправильном вводе)
 
+  /*
+  Тестовый метод для генерации новых слов
+  const generateWord = () => {
+      let word = ''
+      const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+      for( let i=0; i < 5; i++ )
+      word += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+
+      setEndWord('')
+      setStartWord(word)
+  }*/
+
+  const CurrectInput = () => { //Логика при правильном вводе
     /*
-    Тестовый метод для генерации новых слов
-    const generateWord = () => {
-        let word = ''
-        const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    Проверял, почему пробел не отображается
+    if ( startWord[1] == ' ') { 
+        console.log(startWord.substring(1))
+    }
+    */
+    if (startWord.length < 1) { //Проверяет, закончилось ли слово
+      //generateWord()
+    } else {
+      startWord.substring(1)
+      setStartWord(startWord => startWord.substring(1)) //Обновляет значение startWord
+      setEndWord(word => word + startWord[0]) //Обновляет значение endWord
 
-        for( let i=0; i < 5; i++ )
-        word += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    }
+  };
+  useEffect(() => { //Отслеживает нажатие на кнопку
+    const keyDownHandler = event => {
+      // stringId.current.classList.add('shake')
+      if (event.key === startWord[0]) { //Проверяет, верно ли пользователь нажал на кнопку
+        event.preventDefault();
+        CurrectInput(); // Вызывает метод с логикой
+      } else {
+        stringId.current.classList.add('shake') // Трясет строку в случае, если введено неправильное значение
+        setTimeout(() => {
+          stringId.current.classList.remove('shake') //Убирает класс анимации
+        }, 800)
+      }
+    };
 
-        setEndWord('')
-        setStartWord(word)
-    }*/
+    document.addEventListener('keydown', keyDownHandler); // Добавляет слушатель события при нажатии
 
-    const CurrectInput = () => { //Логика при правильном вводе
-        /*
-        Проверял, почему пробел не отображается
-        if ( startWord[1] == ' ') { 
-            console.log(startWord.substring(1))
-        }
-        */
-        if (startWord.length < 1) { //Проверяет, закончилось ли слово
-            //generateWord()
-        } else {
-            startWord.substring(1)
-            setStartWord(startWord => startWord.substring(1)) //Обновляет значение startWord
-            setEndWord(word => word +startWord[0]) //Обновляет значение endWord
-    
-        }
-      };
-      useEffect(() => { //Отслеживает нажатие на кнопку
-        const keyDownHandler = event => {
-        // stringId.current.classList.add('shake')
-          if (event.key === startWord[0]) { //Проверяет, верно ли пользователь нажал на кнопку
-            event.preventDefault();
-            CurrectInput(); // Вызывает метод с логикой
-          } else {
-            stringId.current.classList.add('shake') // Трясет строку в случае, если введено неправильное значение
-            setTimeout(()=> {
-                stringId.current.classList.remove('shake') //Убирает класс анимации
-            }, 800)
-          }
-        };
-    
-        document.addEventListener('keydown', keyDownHandler); // Добавляет слушатель события при нажатии
-    
-        return () => {
-          document.removeEventListener('keydown', keyDownHandler); // Убирает слушатель события при нажатии
-        };
-      }, [startWord]);
-    return (
-        <div className="running-string" ref={stringId}>
-            <div className="input-text end-string">{endWord}</div>
-            <div className="input-text start-string">{startWord}</div>
-        </div>
-    )
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler); // Убирает слушатель события при нажатии
+    };
+  }, [startWord]);
+  return (
+    <div className="container">
+      <p className="demo-task">ДЕМО-ЗАДАНИЕ</p>
+      <div className="running-string" ref={stringId}>
+        <div className="input-text end-string">{endWord}</div>
+        <div className="input-text start-string">{startWord}</div>
+      </div>
+    </div>
+
+  )
 }
 
 export default RunningString;
