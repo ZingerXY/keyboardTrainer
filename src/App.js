c03959059f934ac266ead765f8db9c008a43a724
vladislav <vl_evlashin@mail.ru>
2022-12-21 00:21:08 +0300

    Create first version of page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.scss';
 import Header from "./components/Header/Header";
-import RunningString from './components/RunningString/RunningString';
-import Keyboard from './components/Keyboard/Keyboard';
 import Footer from './components/Footer/Footer';
 import Router from './components/Router/Router';
 
--- src/components/Statistics/components/StatisticsData.jsx
+++ src/components/Statistics/components/StatisticsData.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import './styles/StatisticsData.scss'
 
 const StatisticsData = () => {
+   
+    const [activeBtn, setActiveBtn] = useState(0);
+
+    const setDraw = (e) => {
+        if (activeBtn) {
+            activeBtn.classList.remove('opened')
+            setActiveBtn(e.target)
+            e.target.classList.add('opened')
+        } else {
+            e.target.classList.add('opened')
+            setActiveBtn(e.target)
+        }
+        
+    }
+
     return (
-        <div className="statistics-data"></div>
+        <div className="statistics-data">
+            <h1 className="statistics-header">Общие данные</h1>
+            <div className="statistics-part">
+                <div className="statistics-mini">
+                    <h3 className="statistics-mini-title">Среднее количество ошибок</h3>
+                    <h4 className="statistics-mini-value">10 знаков</h4>
+                </div>
+                <div className="statistics-mini">
+                    <h3 className="statistics-mini-title">Средняя скорость</h3>
+                    <h4 className="statistics-mini-value">400 зн/мин</h4>
+                </div>
+                <div className="statistics-mini">
+                    <h3 className="statistics-mini-title">Аккуратность</h3>
+                    <h4 className="statistics-mini-value">99.99%</h4>
+                </div>
+            </div>
+            <div className="statistics-draw">
+            <h1 className="statistics-header">Графики</h1>
+            <div className="statistics-draw-part">
+                <div className="statistics-draw-selection">
+                    <button className="statistics-draw-btn" onClick={setDraw}>Скорость</button>
+                    <button className="statistics-draw-btn" onClick={setDraw}>Аккуратность</button>
+                    <button className="statistics-draw-btn" onClick={setDraw}>Ошибки</button>
+                </div>
+                <div className="statistics-draw-graph">
+                    <img src="./img/Frame 32.png" alt="" />
+                </div>
+            </div>
+            </div>
+        </div>
     )
 }
 
