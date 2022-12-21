7027c7036216a56fcc312b7c451b1d08e2791b07
SveTi4H <studymail2.7.53@gmail.com>
2022-12-21 23:21:20 +0700

    Add styles for header and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,7 @@ const Footer = () => {
   return (
     <footer className="App-footer">
       <div className="footer container">
-        <p>2022 - Дипломный проект “Клавиатурный тренажер”</p>
-
+        <p className={"footer_text"}>2022 - Дипломный проект “Клавиатурный тренажер”</p>
       </div>
 
     </footer>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Header.scss'
 const Header = () => {
   return (
     <header className="App-header">
-      <div className="nav container">
-        <div className="nav_logo">
-          <img className='logo_img' src="./img/LOGO.svg" alt="logo" />
+      <nav className="nav container">
+        <Link to={"/"} className="nav_logo">
+          <div className="logo_img">
+            <img className="logo" src="./img/LOGO.svg" alt="logo" />
+          </div>
           <div className="logo_name">
             <h1 className="logo_name_item">KANGAROO</h1>
             <p className="logo_title">КЛАВИАТУРНЫЙ ТРЕНАЖЕР</p>
           </div>
-        </div>
+        </Link>
 
         {/* пока временно для демо версии */}
         <div className="nav_links">
-          <h2 className="nav_links_title">Задания</h2>
-          <h2 className="nav_links_title">Статистика</h2>
-          <h2 className="nav_links_title">Выйти</h2>
+          <Link to={"/tasks"} className="nav_links_title">Задания</Link>
+          <Link to={"/stats"} className="nav_links_title">Статистика</Link>
+          <Link to={"/rating"} className="nav_links_title">Рейтинг</Link>
+          <Link to={"/quit"} className="nav_links_title">Выйти</Link>
         </div>
-      </div>
+      </nav>
 
     </header>
   );
