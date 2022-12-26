34895dd219f2923d796d254a27b1395c04c8f7cb
SveTi4H <studymail2.7.53@gmail.com>
2022-12-26 21:24:44 +0700

    Add cards for tasks

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Card.scss"
+const Card = (props) => {
+  let levelArr = [0, 0, 0, 0, 0];
+  for (let i = 0; i < props.level; i++) {
+    levelArr[i] = 1;
+  }
+  const handlerClick = () => {
+    props.state(true);
+  };
+  return (
+    <div className="card" key={props.myKey} onClick={handlerClick}>
+      <div className={"card-head card-head_" + props.type}>
+        <div className="card-name">{props.name}</div>
+        <div className={"card-type card-type_" + props.type}>{props.typeName}</div>
+      </div>
+      <div className="card-bottom">
+        <p className="card-level">Сложность {props.level}</p>
+        <ul className="card-level-box">
+          {levelArr.map((el, id)=>(
+            el === 1
+            ? <li key={id} className="level-point level-active"></li>
+            : <li key={id} className="level-point"></li>
+          ))
+          }
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,14 +1,130 @@
-import React from "react"
+import React, { useState} from 'react';
 import './Tasks.scss'
 import Task from "../Task/Task"
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import Card from "../Card/Card";
 
 const Tasks = () => {
+  const tasksInputObj = [
+    {"id":1,
+      "level": 2,
+      "typeName": "База",
+      "type": "base",
+      "name": "ва ол",
+      "text": "ваол олва овал олол ваол олов овло"},
+    {"id":2,
+      "level": 3,
+      "typeName": "База",
+      "type": "base",
+      "name": "ва ол",
+      "text": "ваол олва овал олол ваол олов овло"},
+    {"id":3,
+      "level": 5,
+      "typeName": "слова",
+      "type": "words",
+      "name": "ва ол",
+      "text": "ваол олва овал олол ваол олов овло"},
+    {"id":4,
+      "level": 4,
+      "typeName": "Знаки",
+      "type": "punctuation",
+      "name": "./ ,?",
+      "text": "ваол олва овал олол ваол олов овло"},
+    {"id":5,
+      "level": 3,
+      "typeName": "Цифры",
+      "type": "numbers",
+      "name": "12 34",
+      "text": "ваол олва овал олол ваол олов овло"},
+  ];
+  let tasksOutputObj = [];
+
+  const [ taskActive, setTaskActive] = useState(false);
+  const [ sort, setSort ] = useState({value: 'desc', text: 'Сначала легкие'});
+  const [ base, setBase] = useState(true);
+  const [ words, setWords] = useState(true);
+  const [ punctuation, setPunctuation] = useState(true);
+  const [ numAndSymbols, setNumAndSymbols] = useState(true);
+
+  const handleStates = () => {
+    tasksOutputObj = [];
+    if (base) {
+      tasksOutputObj = tasksOutputObj.concat(tasksInputObj.filter((el)=> (el.type === "base")));
+    }
+    if (words) {
+      tasksOutputObj = tasksOutputObj.concat(tasksInputObj.filter((el)=> (el.type === "words")));
+    }
+    if (punctuation) {
+      tasksOutputObj = tasksOutputObj.concat(tasksInputObj.filter((el)=> (el.type === "punctuation")));
+    }
+    if (numAndSymbols) {
+      tasksOutputObj = tasksOutputObj.concat(tasksInputObj.filter((el)=> (el.type === "numbers")));
+    }
+    tasksOutputObj.sort((a, b) => {
+      return sort.value === "desc" ? a.level - b.level : b.level - a.level;
+    });
+  };
+
+  const selectOpen = () => {
+    const selectSingle = document.querySelector('.__select');
+    if ('active' === selectSingle.getAttribute('data-state')) {
+      selectSingle.setAttribute('data-state', '');
+    } else {
+      selectSingle.setAttribute('data-state', 'active');
+    }
+  };
+  const selectChange = (event) => {
+    const selectSingle = document.querySelector('.__select');
+    setSort(()=>({value: event.target.getAttribute('data-value'), text: event.target.textContent}));
+    selectSingle.setAttribute('data-state', '');
+  };
+  handleStates();
+  if (taskActive) {
     return (
-        <div className="tasks-page">
-            <Task />
+      <Task />
+    );
+  } else {
+    return (
+      <div className="tasks-page">
+        <div className="tasks-container container">
+          <form action="#" id="filters-form" onChange={handleStates}>
+            <h4 className="filters-title">Тип задания</h4>
+            <div id="brands">
+              <div className="filters-checkbox">
+                <input type="checkbox" className="filters-checkbox_custom" id="base" checked={base} onChange={() => setBase(!base)}/>
+                <label htmlFor="base">Базовые уроки</label>
+              </div>
+              <div className="filters-checkbox">
+                <input type="checkbox" className="filters-checkbox_custom" id="words" checked={words} onChange={() => setWords(!words)}/>
+                <label htmlFor="words"> Слова</label>
+              </div>
+              <div className="filters-checkbox">
+                <input type="checkbox" className="filters-checkbox_custom" id="punctuation" checked={punctuation} onChange={() => setPunctuation(!punctuation)}/>
+                <label htmlFor="punctuation"> Знаки препинания</label>
+              </div>
+              <div className="filters-checkbox">
+                <input type="checkbox" className="filters-checkbox_custom" id="numbers" checked={numAndSymbols} onChange={() => setNumAndSymbols(!numAndSymbols)}/>
+                <label htmlFor="numbers"> Цифры и символы</label>
+              </div>
+            </div>
+            <h4 className="filters-title">Сортировка</h4>
+            <div className="__select" data-state="" onClick={selectOpen}>
+              <div className="__select__title">{sort.text}</div>
+              <div className="__select__content" >
+                <input id="singleSelect0" className="__select__input" type="radio" name="singleSelect" defaultChecked={true}/>
+                <label htmlFor="singleSelect0" tabIndex="0" className="__select__label" data-value="desc" onClick={selectChange}>Сначала легкие</label>
+                <input id="singleSelect1" className="__select__input" type="radio" name="singleSelect"/>
+                <label htmlFor="singleSelect1" tabIndex="0" className="__select__label" data-value="asc" onClick={selectChange}>Сначала сложные</label>
+              </div>
+            </div>
+          </form>
+          <div className="cards-box">
+            {tasksOutputObj.map((el) => (<Card {...el} key={el.id} myKey={el.id} state={setTaskActive}/>))}
+          </div>
         </div>
+      </div>
     )
+  }
 }
 
 export default Tasks
