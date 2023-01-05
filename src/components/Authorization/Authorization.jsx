0250ca571c54462ca72f160ecc4f7224f28c7b45
EvgeniyKorshov <80601543+EvgeniyKorshov@users.noreply.github.com>
2023-01-06 00:02:36 +0300

    corrections
    Для проблем с крестиком используйте документацию:
    https://fontawesome.com/docs/web/use-with/react/
    1.Убрал рамку
    2.Добавил курсор
    3.Добавил отступы и размер текста

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import './Authorization.scss'
-
+import './Authorization.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark} from "@fortawesome/free-solid-svg-icons";
 
@@ -9,16 +8,19 @@ const Authorization = () => {
     <div className="content">
         <div className="window"> 
         <div className="Xmark">
-            <FontAwesomeIcon icon={faXmark} color="#00A876"  size="1x"/>
+          <div className="X">
+          <FontAwesomeIcon icon={faXmark} color="#00A876"  size="1x"/>
+          </div>
+            
             </div>
             <div className="form">
                 <form>
                     <label>
                     <p className="reg-text margin"> АВТОРИЗАЦИЯ</p>
-                    <p className="input-text margin">EMAIL:</p>
-                    <input className="margin" type="email" name="email"  placeholder="test@test.ru"/>
+                    <p className="input-text margin ">EMAIL:</p>
+                    <input className="margin input" type="email" name="email"  placeholder="test@test.ru"/>
                     <p className="input-text margin">ПАРОЛЬ:</p>
-                    <input className="margin" type="password" name="password"  placeholder="12345678"/>
+                    <input className="margin input" type="password" name="password"  placeholder="12345678"/>
                     </label>
                     <p><input className="btn margin" type="submit" value="ВХОД" /></p>
                 </form>
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './Registration.scss';
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark} from "@fortawesome/free-solid-svg-icons";
 
@@ -9,20 +8,22 @@ const Registration = () => {
     <div className="content">
         <div className="window"> 
         <div className="Xmark">
-            <FontAwesomeIcon icon={faXmark} color="#00A876"  size="1x"/>
+            <div className="X">
+            <FontAwesomeIcon  icon={faXmark} color="#00A876"  size="1x"/>
+              </div>
             </div>
             <div className="form">
             <form>
                 <label>
                 <p className="reg-text margin"> РЕГИСТРАЦИЯ</p>
                 <p className="input-text margin">ИМЯ:</p> 
-                <input className="margin" type="text" name="name" placeholder="ПЕТЯ" />
+                <input className="margin input" type="text" name="name" placeholder="ПЕТЯ" />
                 <p className="input-text margin">EMAIL:</p>
-                <input className="margin" type="email" name="email" placeholder="test@test.ru" />
+                <input className="margin input" type="email" name="email" placeholder="test@test.ru" />
                 <p className="input-text margin">ПАРОЛЬ:</p>
-                <input className="margin" type="password" name="pass" placeholder="12345678" />
+                <input className="margin input" type="password" name="pass" placeholder="12345678" />
                 <p className="input-text margin">  ПОДТВЕРЖДЕНИЕ:</p>
-                <input className="margin" type="password" name="confirmpass" placeholder="12345678" />
+                <input className="margin input" type="password" name="confirmpass" placeholder="12345678" />
                 </label>
                 <p><input className="btn margin" type="submit" value="ЗАРЕГИСТРИРОВАТЬСЯ" /></p>
             </form>
