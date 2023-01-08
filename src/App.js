d141f5a5c02aba4eae386a1603b318ba4ecfd7ac
Your Namemaksimovegor401@gmail.com <you@example.com>
2023-01-08 16:00:21 +0000

    Resolving conflict 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import Header from "./components/Header/Header";
 import Footer from './components/Footer/Footer';
-import Router from './routes/uter';
+import Router from './routes/Router';
 
 function App() {
   return (
