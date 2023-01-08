0a6884a05c4f088fa275c19c45088e1e3f95c0ca
SveTi4 <studymail2.7.53@gmail.com>
2023-01-08 22:43:12 +0300

    Change scss on module.scss

--- src/routes/Registration/Registration.jsx
+++ src/routes/Registration/Registration.jsx
@@ -1,31 +1,31 @@
 import React from "react";
-import './Registration.scss';
+import Style from "./Registration.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark} from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Registration = () => {
   return (
-    <div className="content">
-      <div className="window"> 
-        <div className="Xmark">
-          <div className="X">
+    <div className={`content`}>
+      <div className={`${Style["window"]}`}> 
+        <div className={`${Style["Xmark"]}`}>
+          <div className={`${Style["X"]}`}>
             <FontAwesomeIcon  icon={faXmark} color="#00A876"  size="1x"/>
           </div>
         </div>
-        <div className="form">
+        <div className={`${Style["form"]}`}>
           <form>
               <label>
-                <p className="reg-text margin"> РЕГИСТРАЦИЯ</p>
-                <p className="input-text margin">ИМЯ:</p> 
-                <input className="margin input" type="text" name="name" placeholder="ПЕТЯ" />
-                <p className="input-text margin">EMAIL:</p>
-                <input className="margin input" type="email" name="email" placeholder="test@test.ru" />
-                <p className="input-text margin">ПАРОЛЬ:</p>
-                <input className="margin input" type="password" name="pass" placeholder="12345678" />
-                <p className="input-text margin">  ПОДТВЕРЖДЕНИЕ:</p>
-                <input className="margin input" type="password" name="confirmpass" placeholder="12345678" />
+                <p className={`${Style["reg-text"]} ${Style["margin"]}`}> РЕГИСТРАЦИЯ</p>
+                <p className={`${Style["input-text"]} ${Style["margin"]}`}>ИМЯ:</p> 
+                <input className={`${Style["margin"]} ${Style["input"]}`} type="text" name="name" placeholder="ПЕТЯ" />
+                <p className={`${Style["input-text"]} ${Style["margin"]}`}>EMAIL:</p>
+                <input className={`${Style["margin"]} ${Style["input"]}`} type="email" name="email" placeholder="test@test.ru" />
+                <p className={`${Style["input-text"]} ${Style["margin"]}`}>ПАРОЛЬ:</p>
+                <input className={`${Style["margin"]} ${Style["input"]}`} type="password" name="pass" placeholder="12345678" />
+                <p className={`${Style["input-text"]} ${Style["margin"]}`}>  ПОДТВЕРЖДЕНИЕ:</p>
+                <input className={`${Style["margin"]} ${Style["input"]}`} type="password" name="confirmpass" placeholder="12345678" />
               </label>
-              <p><input className="btn margin" type="submit" value="ЗАРЕГИСТРИРОВАТЬСЯ" /></p>
+              <p><input className={`${Style["btn"]} ${Style["margin"]}`} type="submit" value="ЗАРЕГИСТРИРОВАТЬСЯ" /></p>
           </form>
         </div>
       </div>
