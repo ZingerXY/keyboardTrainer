4b4fcaa8c332f02f26869836bb7ecbf56acb1b9a
SveTi4 <studymail2.7.53@gmail.com>
2023-01-09 00:41:21 +0300

    Change scss on module.scss

--- src/routes/Authorization/Authorization.jsx
+++ src/routes/Authorization/Authorization.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import './Authorization.scss';
+import Style from "./Authorization.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark} from "@fortawesome/free-solid-svg-icons";
 
 const Authorization = () => {
   return (
-    <div className="content">
-      <div className="window"> 
-        <div className="Xmark">
-          <div className="X">
+    <div className={`content`}>
+      <div className={`${Style["window"]}`}> 
+        <div className={`${Style["Xmark"]}`}>
+          <div className={`${Style["X"]}`}>
             <FontAwesomeIcon icon={faXmark} color="#00A876"  size="1x"/>
           </div>
         </div>
-        <div className="form">
+        <div className={`${Style["form"]}`}>
           <form>
             <label>
-            <p className="reg-text margin"> АВТОРИЗАЦИЯ</p>
-            <p className="input-text margin ">EMAIL:</p>
-            <input className="margin input" type="email" name="email"  placeholder="test@test.ru"/>
-            <p className="input-text margin">ПАРОЛЬ:</p>
-            <input className="margin input" type="password" name="password"  placeholder="12345678"/>
+            <p className={`${Style["reg-text"]} ${Style["margin"]}`}> АВТОРИЗАЦИЯ</p>
+            <p className={`${Style["input-text"]} ${Style["margin"]}`}>EMAIL:</p>
+            <input className={`${Style["margin"]} ${Style["input"]}`} type="email" name="email"  placeholder="test@test.ru"/>
+            <p className={`${Style["input-text"]} ${Style["margin"]}`}>ПАРОЛЬ:</p>
+            <input className={`${Style["margin"]} ${Style["input"]}`} type="password" name="password"  placeholder="12345678"/>
             </label>
-            <p><input className="btn margin" type="submit" value="ВХОД" /></p>
+            <p><input className={`${Style["btn"]} ${Style["margin"]}`} type="submit" value="ВХОД" /></p>
           </form>
-          <p className="reg-black margin">Если у вас еще нет аккаунта <a href="#">зарегистрируйтесь в системе</a></p> 
+          <p className={`${Style["reg-black"]} ${Style["margin"]}`}>Если у вас еще нет аккаунта <a href="#">зарегистрируйтесь в системе</a></p> 
         </div>
       </div>
     </div>
