fcc0c29e450953978d7eabc090e5d694c295c904
SveTi4 <studymail2.7.53@gmail.com>
2023-01-08 22:52:28 +0300

    Change scss on module.scss

--- src/routes/Rating/Rating.jsx
+++ src/routes/Rating/Rating.jsx
@@ -1,58 +1,58 @@
 import React from "react";
-import './Rating.scss';
+import Style from "./Rating.module.scss";
 
 const Rating = () => {
   return (
-    <div className="rating-page">
-      <div className="container rating-container">
-        <h2 className="rating-title">Рейтинг пользователей</h2>
-        <div className="rating-box">
-          <div className="rating-item rating-item_first">
-              <div className="rating-position">1</div>
-            <div className="rating-name">Иванов Иван</div>
-            <div className="rating-stats">
-              <div className="rating-percent">99.99%</div>
-              <div className="rating-charMin">400 зн/мин</div>
+    <div className={`${Style["rating-page"]}`}>
+      <div className={`container ${Style["rating-container"]}`}>
+        <h2 className={`${Style["rating-title"]}`}>Рейтинг пользователей</h2>
+        <div className={`${Style["rating-box"]}`}>
+          <div className={`${Style["rating-item"]} ${Style["rating-item_first"]}`}>
+              <div className={`${Style["rating-position"]}`}>1</div>
+            <div className={`${Style["rating-name"]}`}>Иванов Иван</div>
+            <div className={`${Style["rating-stats"]}`}>
+              <div className={`${Style["rating-percent"]}`}>99.99%</div>
+              <div className={`${Style["rating-charMin"]}`}>400 зн/мин</div>
             </div>
           </div>
-          <div className="rating-item">
-            <div className="rating-position">196</div>
-            <div className="rating-name">Иванов Иван</div>
-            <div className="rating-stats">
-              <div className="rating-percent">99.99%</div>
-              <div className="rating-charMin">400 зн/мин</div>
+          <div className={`${Style["rating-item"]}`}>
+            <div className={`${Style["rating-position"]}`}>196</div>
+            <div className={`${Style["rating-name"]}`}>Иванов Иван</div>
+            <div className={`${Style["rating-stats"]}`}>
+              <div className={`${Style["rating-percent"]}`}>99.99%</div>
+              <div className={`${Style["rating-charMin"]}`}>400 зн/мин</div>
             </div>
           </div>
-          <div className="rating-item">
-            <div className="rating-position">197</div>
-            <div className="rating-name">Иванов Иван</div>
-            <div className="rating-stats">
-              <div className="rating-percent">99.99%</div>
-              <div className="rating-charMin">400 зн/мин</div>
+          <div className={`${Style["rating-item"]}`}>
+            <div className={`${Style["rating-position"]}`}>197</div>
+            <div className={`${Style["rating-name"]}`}>Иванов Иван</div>
+            <div className={`${Style["rating-stats"]}`}>
+              <div className={`${Style["rating-percent"]}`}>99.99%</div>
+              <div className={`${Style["rating-charMin"]}`}>400 зн/мин</div>
             </div>
           </div>
-          <div className="rating-item rating-item_p">
-            <div className="rating-position">198</div>
-            <div className="rating-name">Иванов Иван</div>
-            <div className="rating-stats">
-              <div className="rating-percent">99.99%</div>
-              <div className="rating-charMin">400 зн/мин</div>
+          <div className={`${Style["rating-item"]} ${Style["rating-item_p"]}`}>
+            <div className={`${Style["rating-position"]}`}>198</div>
+            <div className={`${Style["rating-name"]}`}>Иванов Иван</div>
+            <div className={`${Style["rating-stats"]}`}>
+              <div className={`${Style["rating-percent"]}`}>99.99%</div>
+              <div className={`${Style["rating-charMin"]}`}>400 зн/мин</div>
             </div>
           </div>
-          <div className="rating-item">
-            <div className="rating-position">199</div>
-            <div className="rating-name">Иванов Иван</div>
-            <div className="rating-stats">
-              <div className="rating-percent">99.99%</div>
-              <div className="rating-charMin">400 зн/мин</div>
+          <div className={`${Style["rating-item"]}`}>
+            <div className={`${Style["rating-position"]}`}>199</div>
+            <div className={`${Style["rating-name"]}`}>Иванов Иван</div>
+            <div className={`${Style["rating-stats"]}`}>
+              <div className={`${Style["rating-percent"]}`}>99.99%</div>
+              <div className={`${Style["rating-charMin"]}`}>400 зн/мин</div>
             </div>
           </div>
-          <div className="rating-item">
-            <div className="rating-position">200</div>
-            <div className="rating-name">Иванов Иван</div>
-            <div className="rating-stats">
-              <div className="rating-percent">99.99%</div>
-              <div className="rating-charMin">400 зн/мин</div>
+          <div className={`${Style["rating-item"]}`}>
+            <div className={`${Style["rating-position"]}`}>200</div>
+            <div className={`${Style["rating-name"]}`}>Иванов Иван</div>
+            <div className={`${Style["rating-stats"]}`}>
+              <div className={`${Style["rating-percent"]}`}>99.99%</div>
+              <div className={`${Style["rating-charMin"]}`}>400 зн/мин</div>
             </div>
           </div>
         </div>
