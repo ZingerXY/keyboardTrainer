87952a4ab6ed1b0c1b79a51a957d6216dba55938
SveTi4 <studymail2.7.53@gmail.com>
2023-01-09 07:32:36 +0300

    Fix Graph width

--- src/routes/Statistics/Statistics.jsx
+++ src/routes/Statistics/Statistics.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Style from "./Statistics.module.scss";
 import StatisticsData from "../../components/StatisticsData/StatisticsData";
 import GraphComponent from "../../components/GraphComponent/GraphComponent";
 
 const Statistics = () => {
     return (
-        <div className="statistics">
+        <div className={`${Style["statistics"]} container`}>
             <StatisticsData />
             <GraphComponent />
         </div>
