a03f7922530d9b6b10b27749f2b31a9f027e7a5c
Your Namemaksimovegor401@gmail.com <you@example.com>
2023-01-16 14:02:28 +0000

    all fixed

--- src/routes/Tasks/Tasks.jsx
+++ src/routes/Tasks/Tasks.jsx
@@ -1,12 +1,11 @@
-import React, { useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Style from "./Tasks.module.scss";
 import Task from "../../components/Task/Task"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Card from "../../components/Card/Card";
 
-const Tasks = () => {
-  const tasksInputObj = [
-    // {
+const initialTasks = JSON.parse(JSON.stringify([
+   // {
     //   "id":1,
     //   "level": 2,
     //   "typeName": "База",
@@ -51,56 +50,58 @@ const Tasks = () => {
       "text": "ваол олва овал олол ваол олов овло",
       "howManyToGenerate": 10
     },
-  ];
-  let tasksOutputObj = [];
+]));
 
-  const [ isTaskChosen, setIsTaskChosen] = useState(false);
-  const [taskOption, setTaskOption] = useState('')
-  const [ sort, setSort ] = useState({value: 'desc', text: 'Сначала легкие'});
-  const [ base, setBase] = useState(true);
-  const [ words, setWords] = useState(true);
-  const [ punctuation, setPunctuation] = useState(true);
-  const [ numAndSymbols, setNumAndSymbols] = useState(true);
+const Tasks = () => {
+  const [tasksOutputObj, setTasksOutputObj] = useState([]);
 
-  const handleStates = () => {
-    tasksOutputObj = [];
-    if (base) {
-      tasksOutputObj = tasksOutputObj.concat(tasksInputObj.filter((el)=> (el.type === "base")));
-    }
-    if (words) {
-      tasksOutputObj = tasksOutputObj.concat(tasksInputObj.filter((el)=> (el.type === "words")));
-    }
-    if (punctuation) {
-      tasksOutputObj = tasksOutputObj.concat(tasksInputObj.filter((el)=> (el.type === "punctuation")));
-    }
-    if (numAndSymbols) {
-      tasksOutputObj = tasksOutputObj.concat(tasksInputObj.filter((el)=> (el.type === "numbers")));
-    }
-    tasksOutputObj.sort((a, b) => {
-      return sort.value === "desc" ? a.level - b.level : b.level - a.level;
-    });
-  };
+  const [taskOption, setTaskOption] = useState('');
+  const [taskActive, setTaskActive] = useState(false);
+  const [sort, setSort] = useState({value: 'desc', text: 'Сначала легкие'});
+  const [base, setBase] = useState(true);
+  const [words, setWords] = useState(true);
+  const [punctuation, setPunctuation] = useState(true);
+  const [numAndSymbols, setNumAndSymbols] = useState(true);
+  const [selectState, setSelectState] = useState(true);
 
   const selectOpen = () => {
-    const selectSingle = document.querySelector(`.${Style["__select"]}`);
-    if ('active' === selectSingle.getAttribute('data-state')) {
-      selectSingle.setAttribute('data-state', '');
-    } else {
-      selectSingle.setAttribute('data-state', 'active');
-    }
+    setSelectState(!selectState)
+  }
+
+  const selectChange = (newValue, newText, e) => {
+    setSort({value: newValue, text: newText});
+    if (e) e.stopPropagation()
   };
-  const selectChange = (event) => {
-    const selectSingle = document.querySelector(`.${Style["__select"]}`);
-    setSort(()=>({value: event.target.getAttribute('data-value'), text: event.target.textContent}));
-    selectSingle.setAttribute('data-state', '');
-  };
-  handleStates();
-  if (isTaskChosen) {
-    const task = tasksInputObj.filter(el => el.type === taskOption)[0];
+
+  const sortTasks = (a, b) => sort.value === "desc" ? a.level - b.level : b.level - a.level
+
+  useEffect(() => {
+    const filterTypes = []
+    if (base) filterTypes.push("base")
+    if (words) filterTypes.push("words")
+    if (punctuation) filterTypes.push("punctuation")
+    if (numAndSymbols) filterTypes.push("numbers")
+
+    setTasksOutputObj([
+        ...initialTasks
+        .filter(el => filterTypes.includes(el.type))
+        .sort(sortTasks)
+      ]
+    )
+  }, [base, words, punctuation, numAndSymbols])
+
+  useEffect(() => {
+    setTasksOutputObj(prev => [...prev.sort(sortTasks)])
+  }, [sort])
+
+  if (taskActive) {
+    const task = initialTasks.filter(el => el.type === taskOption)[0];
     const taskSettings = {
       type: taskOption,
       amount: task.howManyToGenerate
     }
+    console.log(task, taskSettings);
+
     return (
       <Task
         taskSettings={taskSettings}
@@ -110,51 +111,58 @@ const Tasks = () => {
     return (
       <div className={`${Style["tasks-page"]}`}>
         <div className={`container ${Style["tasks-container"]}`}>
-          <form action="#" className={`${Style["filters-form"]}`} onChange={handleStates}>
+          <form action="#" className={`${Style["filters-form"]}`}>
             <div className={`${Style["form-checkboxs"]}`}>
               <h4 className={`${Style["filters-title"]}`}>Тип задания</h4>
               <div>
                 <div className={`${Style["filters-checkbox"]}`}>
-                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="base" checked={base} onChange={() => setBase(!base)}/>
+                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="base" checked={base}
+                         onChange={() => setBase(!base)}/>
                   <label htmlFor="base">Базовые уроки</label>
                 </div>
                 <div className={`${Style["filters-checkbox"]}`}>
-                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="words" checked={words} onChange={() => setWords(!words)}/>
+                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="words" checked={words}
+                         onChange={() => setWords(!words)}/>
                   <label htmlFor="words"> Слова</label>
                 </div>
                 <div className={`${Style["filters-checkbox"]}`}>
-                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="punctuation" checked={punctuation} onChange={() => setPunctuation(!punctuation)}/>
+                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="punctuation"
+                         checked={punctuation} onChange={() => setPunctuation(!punctuation)}/>
                   <label htmlFor="punctuation"> Знаки препинания</label>
                 </div>
                 <div className={`${Style["filters-checkbox"]}`}>
-                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="numbers" checked={numAndSymbols} onChange={() => setNumAndSymbols(!numAndSymbols)}/>
+                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="numbers"
+                         checked={numAndSymbols} onChange={() => setNumAndSymbols(!numAndSymbols)}/>
                   <label htmlFor="numbers"> Цифры и символы</label>
                 </div>
               </div>
             </div>
             <div className={`${Style["form-selector"]}`}>
               <h4 className={`${Style["filters-title"]}`}>Сортировка</h4>
-              <div className={`${Style["__select"]}`} data-state="" onClick={selectOpen}>
+              <div className={`${Style["__select"]} ${selectState ? Style["__select_active"] : ""}`}
+                   onClick={selectOpen}>
                 <div className={`${Style["__select__title"]}`}>{sort.text}</div>
                 <div className={`${Style["__select__content"]}`}>
-                  <input id="singleSelect0" className={`${Style["__select__input"]}`} type="radio" name="singleSelect" defaultChecked={true}/>
-                  <label htmlFor="singleSelect0" tabIndex="0" className={`${Style["__select__label"]}`} data-value="desc" onClick={selectChange}>Сначала легкие</label>
+                  <input id="singleSelect0" className={`${Style["__select__input"]}`} type="radio" name="singleSelect"
+                         defaultChecked={true}/>
+                  <label htmlFor="singleSelect0" tabIndex="0" className={`${Style["__select__label"]}`}
+                         onClick={e => selectChange("desc", "Сначала легкие", e)}>Сначала легкие</label>
                   <input id="singleSelect1" className={`${Style["__select__input"]}`} type="radio" name="singleSelect"/>
-                  <label htmlFor="singleSelect1" tabIndex="0" className={`${Style["__select__label"]}`} data-value="asc" onClick={selectChange}>Сначала сложные</label>
+                  <label htmlFor="singleSelect1" tabIndex="0" className={`${Style["__select__label"]}`}
+                         onClick={e => selectChange("asc", "Сначала сложные", e)}>Сначала сложные</label>
                 </div>
               </div>
             </div>
           </form>
           <div className={`${Style["cards-box"]}`}>
-            {tasksOutputObj.map((el) => (
+            {tasksOutputObj.map((el) => 
               <Card 
                 {...el}
                 key={el.id}
                 myKey={el.id}
-                state={setIsTaskChosen}
+                state={setTaskActive}
                 setTaskOption={setTaskOption}
-              />
-            ))}
+              />)}
           </div>
         </div>
       </div>
