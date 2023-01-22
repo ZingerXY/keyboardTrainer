a0405236c645ddf195e92156c74dce33f23bb899
vladislav <vl_evlashin@mail.ru>
2023-01-22 14:05:18 +0300

    Pull Tasks

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {useState} from 'react';
 import Style from "./Card.module.scss";
 
 const Card = (props) => {
   let levelArr = [0, 0, 0, 0, 0];
-  for (let i = 0; i < props.level; i++) {
+  const [type, setType] = useState('')
+  const [name, setName] = useState('')
+  for (let i = 0; i < props.difficulty; i++) {
     levelArr[i] = 1;
   }
+  useEffect(() => {
+    switch (props.task_type) {
+      case 'База':
+        setType('base')
+        setName('ва ол')
+        break;
+        case 'Слова':
+          setType('words')
+          setName('ва ол')
+          break;
+        case 'Знаки':
+          setType('punctuation')
+          setName('./ ,?')
+          break;
+        case 'Цифры':
+          setType('numbers')
+          setName('12 34')
+          break;
+      default:
+        break;
+    }
+  }, [])
   const handlerClick = () => {
-    props.setTaskOption(props.type);
+    props.setTaskOption(type);
     props.state(true);
   };
 
   return (
     <div className={`${Style["card"]}`} key={props.myKey} onClick={handlerClick}>
-      <div className={`${Style["card-head"]} ${Style["card-head_" + props.type]}`}>
-        <div className={`${Style["card-name"]}`}>{props.name}</div>
-        <div className={`${Style["card-type"]} ${Style["card-type_" + props.type]}`}>{props.typeName}</div>
+      <div className={`${Style["card-head"]} ${Style["card-head_" + type]}`}>
+        <div className={`${Style["card-name"]}`}>{name}</div>
+        <div className={`${Style["card-type"]} ${Style["card-type_" + type]}`}>{props.task_type}</div>
       </div>
       <div className={`${Style["card-bottom"]}`}>
         <p className={`${Style["card-level"]}`}>Сложность</p>
