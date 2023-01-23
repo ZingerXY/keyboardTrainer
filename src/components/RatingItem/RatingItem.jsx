db51d0d67e69780cea3a1e40f0515374093be2e2
SveTi4 <studymail2.7.53@gmail.com>
2023-01-23 11:10:17 +0300

    Simple filling rating page

---
+++ src/components/RatingItem/RatingItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Style from "../../routes/Rating/Rating.module.scss";
+
+const RatingItem = (props) => {
+    return(
+        <>
+            <div className={`${Style["rating-item"]} ${props.style}`}>
+            <div className={`${Style["rating-position"]}`}>{props.position}</div>
+            <div className={`${Style["rating-name"]}`}>{props.userName}</div>
+            <div className={`${Style["rating-stats"]}`}>
+                <div className={`${Style["rating-percent"]}`}>-----%</div>
+                <div className={`${Style["rating-charMin"]}`}>----- зн/мин</div>
+            </div>
+            </div>
+        </>
+    );
+};
+
+export default RatingItem;
