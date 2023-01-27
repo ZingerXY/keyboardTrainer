07a2f4190a3aecd111c71ebe1a884db0b970b028
AnastasiyaVi <nastyonaki@mail.ru>
2023-01-27 10:20:45 +0300

    add pagination to tasks page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,56 +1,65 @@
 import React, { useEffect } from "react";
-import {useState} from 'react';
+import { useState } from 'react';
 import Style from "./Card.module.scss";
 
 const Card = (props) => {
   let levelArr = [0, 0, 0, 0, 0];
-  const [type, setType] = useState('')
-  const [name, setName] = useState('')
+  const [type, setType] = useState("");
   for (let i = 0; i < props.difficulty; i++) {
     levelArr[i] = 1;
   }
   useEffect(() => {
     switch (props.task_type) {
-      case 'База':
-        setType('base')
-        setName('ва ол')
+      case "База":
+        setType("base");
         break;
-        case 'Слова':
-          setType('words')
-          setName('ва ол')
-          break;
-        case 'Знаки':
-          setType('punctuation')
-          setName('./ ,?')
-          break;
-        case 'Цифры':
-          setType('numbers')
-          setName('12 34')
-          break;
+      case "Буквы":
+        setType("letters");
+        break;
+      case "Слова":
+        setType("words");
+        break;
+      case "Знаки":
+        setType("punctuation");
+        break;
+      case "Цифры":
+        setType("numbers");
+        break;
       default:
         break;
     }
-  }, [])
+  }, []);
+
   const handlerClick = () => {
     props.setTaskOption(type);
     props.state(true);
   };
 
   return (
-    <div className={`${Style["card"]}`} key={props.myKey} onClick={handlerClick}>
+    <div
+      className={`${Style["card"]}`}
+      key={props.myKey}
+      onClick={handlerClick}
+    >
       <div className={`${Style["card-head"]} ${Style["card-head_" + type]}`}>
-        <div className={`${Style["card-name"]}`}>{name}</div>
-        <div className={`${Style["card-type"]} ${Style["card-type_" + type]}`}>{props.task_type}</div>
+        <div className={`${Style["card-name"]}`}>{props.task}</div>
+        <div className={`${Style["card-type"]} ${Style["card-type_" + type]}`}>
+          {props.task_type}
+        </div>
       </div>
       <div className={`${Style["card-bottom"]}`}>
         <p className={`${Style["card-level"]}`}>Сложность</p>
         <ul className={`${Style["card-level-box"]}`}>
-          {levelArr.map((el, id)=>(
-            el === 1
-            ? <li key={id} className={`${Style["level-point"]} ${Style["level-active"]}`}></li>
-            : <li key={id} className={`${Style["level-point"]}`}></li>
-          ))
-          }
+          {levelArr.map((el, id) =>
+            el === 1 ? (
+              <li
+                key={id}
+                className={`${Style["level-point"]} ${Style["level-active"]}`}
+              ></li>
+            ) : (
+              <li key={id} className={`${Style["level-point"]}`}></li>
+            )
+          )}
         </ul>
       </div>
     </div>
--- src/routes/Tasks/Tasks.jsx
+++ src/routes/Tasks/Tasks.jsx
@@ -1,124 +1,127 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Style from "./Tasks.module.scss";
 import Task from "../../components/Task/Task"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Card from "../../components/Card/Card";
 import BasicPagination from "../../components/Pagination/Pagination";
 
-// const initialTasks = JSON.parse(JSON.stringify([
-//    // {
-//     //   "id":1,
-//     //   "level": 2,
-//     //   "typeName": "База",
-//     //   "type": "base",
-//     //   "name": "ва ол",
-//     //   "text": "ваол олва овал олол ваол олов овло",
-//     // "howManyToGenerate": 10
-//     // },
-//     // {
-//     //   "id":2,
-//     //   "level": 3,
-//     //   "typeName": "База",
-//     //   "type": "base",
-//     //   "name": "ва ол",
-//     //   "text": "ваол олва овал олол ваол олов овло",
-//     // "howManyToGenerate": 10
-//     // },
-//     {
-//       "id":3,
-//       "level": 5,
-//       "typeName": "слова",
-//       "type": "words",
-//       "name": "ва ол",
-//       "text": "ваол олва овал олол ваол олов овло",
-//       "howManyToGenerate": 5
-//     },
-// {
-//   "id":4,
-//   "level": 4,
-//   "typeName": "Знаки",
-//   "type": "punctuation",
-//   "name": "./ ,?",
-//   "text": "ваол олва овал олол ваол олов овло",
-//   "howManyToGenerate": 7
-// },
-//     {
-//       "id":5,
-//       "level": 3,
-//       "typeName": "Цифры",
-//       "type": "numbers",
-//       "name": "12 34",
-//       "text": "ваол олва овал олол ваол олов овло",
-//       "howManyToGenerate": 10
-//     },
-// ]));
-
 const Tasks = () => {
-  const [tasksOutputObj, setTasksOutputObj] = useState([]);
   const [initialTasks, setInitialTasks] = useState([]);
-  const [taskOption, setTaskOption] = useState('');
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  const [taskOption, setTaskOption] = useState("");
   const [taskActive, setTaskActive] = useState(false);
-  const [sort, setSort] = useState({value: 'desc', text: 'Сначала легкие'});
+  const [sort, setSort] = useState({ value: "desc", text: "Сначала легкие" });
   const [base, setBase] = useState(true);
   const [words, setWords] = useState(true);
+  const [letters, setLetters] = useState(true);
   const [punctuation, setPunctuation] = useState(true);
   const [numAndSymbols, setNumAndSymbols] = useState(true);
   const [selectState, setSelectState] = useState(true);
+  const [isLoading, setLoading] = useState(false);
+  const [paginationInfo, setPaginationInfo] = useState({
+    limit: 10,
+    page: 1,
+    totalItems: 0,
+    sort: "asc",
+  });
 
+  const loadPost = async (page) => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://kangaroo.zingery.ru/api/tasks?page=${page}`
+      );
+      const json = await response.json();
 
-  useEffect(() => {
-    const loadPost = async () => {
-      await fetch('https://kangaroo.zingery.ru/api/tasks')
-      .then(data => data.json())
-      .then(res => {
-        setInitialTasks(res.data)
-      }, (e) => {
-      })
+      const filterTypes = getCollectedFilters();
+
+      setInitialTasks(json.data);
+
+      setFilteredTasks(
+        json.data
+          .filter((el) => filterTypes.includes(el.task_type))
+          .sort(sortTasks)
+      );
+
+      setPaginationInfo({
+        ...paginationInfo,
+        page: json.meta.current_page,
+        totalItems: json.meta.total,
+        limit: json.meta.per_page,
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
     }
-    loadPost()
-  }, [])
-
-
-  const selectOpen = () => {
-    setSelectState(!selectState)
-  }
-
-  const selectChange = (newValue, newText, e) => {
-    setSort({value: newValue, text: newText});
-    if (e) e.stopPropagation()
   };
 
-  const sortTasks = (a, b) => sort.value === "desc" ? a.difficulty - b.difficulty : b.difficulty - a.difficulty
+  const selectOpen = () => {
+    setSelectState(!selectState);
+  };
+
+  const selectChange = (newValue, newText, e) => {
+    setSort({ value: newValue, text: newText });
+    if (e) e.stopPropagation();
+  };
+
+  const sortTasks = (a, b) =>
+    sort.value === "desc"
+      ? a.difficulty - b.difficulty
+      : b.difficulty - a.difficulty;
+
+  const getCollectedFilters = () => {
+    const filterTypes = [];
+    if (letters) filterTypes.push("Буквы");
+    if (words) filterTypes.push("Слова");
+    if (base) filterTypes.push("База");
+    if (punctuation) filterTypes.push("Пунктуация");
+    if (numAndSymbols) filterTypes.push("Цифры");
+
+    return filterTypes;
+  };
 
   useEffect(() => {
-    const filterTypes = []
-    // if (base) filterTypes.push("База")
-    if (words) filterTypes.push("Слова")
-    if (punctuation) filterTypes.push("Знаки")
-    if (numAndSymbols) filterTypes.push("Цифры")
-    setTasksOutputObj([
-        ...initialTasks
-        .filter(el => filterTypes.includes(el.task_type))
-        .sort(sortTasks)
-      ]
-    )
-  }, [base, words, punctuation, numAndSymbols, initialTasks])
+    loadPost(paginationInfo.page);
+  }, []);
 
   useEffect(() => {
-    setTasksOutputObj(prev => [...prev.sort(sortTasks)])
-  }, [sort])
+    if (!initialTasks.length) return;
+
+    const filterTypes = getCollectedFilters();
+
+    const newTasks = [
+      ...initialTasks
+        .filter((el) => filterTypes.includes(el.task_type))
+        .sort(sortTasks),
+    ];
+
+    setFilteredTasks(newTasks);
+  }, [base, words, letters, punctuation, numAndSymbols]);
+
+  useEffect(() => {
+    if (!initialTasks.length) return;
+
+    setInitialTasks((prev) => [...prev.sort(sortTasks)]);
+  }, [sort]);
+
+  const handlePageChange = (event, page) => {
+    setPaginationInfo({
+      ...paginationInfo,
+      page,
+    });
+    loadPost(page);
+  };
+
+  if (isLoading) return "Загрузка....";
 
   if (taskActive) {
-    const task = initialTasks.filter(el => el.task_type === taskOption)[0];
+    const task = initialTasks.filter((el) => el.task_type === taskOption)[0];
     const taskSettings = {
       type: taskOption,
-      amount: 7
-    }
-    return (
-      <Task
-        taskSettings={taskSettings}
-      />
-    );
+      amount: 7,
+    };
+    return <Task taskSettings={taskSettings} />;
   } else {
     return (
       <div className={`${Style["tasks-page"]}`}>
@@ -128,61 +131,120 @@ const Tasks = () => {
               <h4 className={`${Style["filters-title"]}`}>Тип задания</h4>
               <div>
                 <div className={`${Style["filters-checkbox"]}`}>
-                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="base" checked={base}
-                         onChange={() => setBase(!base)}/>
+                  <input
+                    type="checkbox"
+                    className={`${Style["filters-checkbox_custom"]}`}
+                    id="base"
+                    checked={base}
+                    onChange={() => setBase(!base)}
+                  />
                   <label htmlFor="base">Базовые уроки</label>
                 </div>
                 <div className={`${Style["filters-checkbox"]}`}>
-                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="words" checked={words}
-                         onChange={() => setWords(!words)}/>
-                  <label htmlFor="words"> Слова</label>
+                  <input
+                    type="checkbox"
+                    className={`${Style["filters-checkbox_custom"]}`}
+                    id="words"
+                    checked={words}
+                    onChange={() => setWords(!words)}
+                  />
+                  <label htmlFor="words">Слова</label>
                 </div>
                 <div className={`${Style["filters-checkbox"]}`}>
-                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="punctuation"
-                         checked={punctuation} onChange={() => setPunctuation(!punctuation)}/>
+                  <input
+                    type="checkbox"
+                    className={`${Style["filters-checkbox_custom"]}`}
+                    id="letters"
+                    checked={letters}
+                    onChange={() => setLetters(!letters)}
+                  />
+                  <label htmlFor="letters">Буквы</label>
+                </div>
+                <div className={`${Style["filters-checkbox"]}`}>
+                  <input
+                    type="checkbox"
+                    className={`${Style["filters-checkbox_custom"]}`}
+                    id="punctuation"
+                    checked={punctuation}
+                    onChange={() => setPunctuation(!punctuation)}
+                  />
                   <label htmlFor="punctuation"> Знаки препинания</label>
                 </div>
                 <div className={`${Style["filters-checkbox"]}`}>
-                  <input type="checkbox" className={`${Style["filters-checkbox_custom"]}`} id="numbers"
-                         checked={numAndSymbols} onChange={() => setNumAndSymbols(!numAndSymbols)}/>
+                  <input
+                    type="checkbox"
+                    className={`${Style["filters-checkbox_custom"]}`}
+                    id="numbers"
+                    checked={numAndSymbols}
+                    onChange={() => setNumAndSymbols(!numAndSymbols)}
+                  />
                   <label htmlFor="numbers"> Цифры и символы</label>
                 </div>
               </div>
             </div>
             <div className={`${Style["form-selector"]}`}>
               <h4 className={`${Style["filters-title"]}`}>Сортировка</h4>
-              <div className={`${Style["__select"]} ${selectState ? Style["__select_active"] : ""}`}
-                   onClick={selectOpen}>
+              <div
+                className={`${Style["__select"]} ${selectState ? Style["__select_active"] : ""
+                  }`}
+                onClick={selectOpen}
+              >
                 <div className={`${Style["__select__title"]}`}>{sort.text}</div>
                 <div className={`${Style["__select__content"]}`}>
-                  <input id="singleSelect0" className={`${Style["__select__input"]}`} type="radio" name="singleSelect"
-                         defaultChecked={true}/>
-                  <label htmlFor="singleSelect0" tabIndex="0" className={`${Style["__select__label"]}`}
-                         onClick={e => selectChange("desc", "Сначала легкие", e)}>Сначала легкие</label>
-                  <input id="singleSelect1" className={`${Style["__select__input"]}`} type="radio" name="singleSelect"/>
-                  <label htmlFor="singleSelect1" tabIndex="0" className={`${Style["__select__label"]}`}
-                         onClick={e => selectChange("asc", "Сначала сложные", e)}>Сначала сложные</label>
+                  <input
+                    id="singleSelect0"
+                    className={`${Style["__select__input"]}`}
+                    type="radio"
+                    name="singleSelect"
+                    defaultChecked={true}
+                  />
+                  <label
+                    htmlFor="singleSelect0"
+                    tabIndex="0"
+                    className={`${Style["__select__label"]}`}
+                    onClick={(e) => selectChange("desc", "Сначала легкие", e)}
+                  >
+                    Сначала легкие
+                  </label>
+                  <input
+                    id="singleSelect1"
+                    className={`${Style["__select__input"]}`}
+                    type="radio"
+                    name="singleSelect"
+                  />
+                  <label
+                    htmlFor="singleSelect1"
+                    tabIndex="0"
+                    className={`${Style["__select__label"]}`}
+                    onClick={(e) => selectChange("asc", "Сначала сложные", e)}
+                  >
+                    Сначала сложные
+                  </label>
                 </div>
               </div>
             </div>
           </form>
           <div className={`${Style["cards-box"]}`}>
-            {tasksOutputObj.map((el) =>
+            {filteredTasks.map((el) => (
               <Card
                 {...el}
                 key={el.id}
                 myKey={el.id}
                 state={setTaskActive}
                 setTaskOption={setTaskOption}
-              />)}
+              />
+            ))}
           </div>
         </div>
         <div className={`${Style["Pagination"]} container`}>
-          <BasicPagination/>
+          <BasicPagination
+            onPageChange={handlePageChange}
+            {...paginationInfo}
+          />
         </div>
       </div>
-    )
+    );
   }
-}
+};
 
-export default Tasks
+export default Tasks;
