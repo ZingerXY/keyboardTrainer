870249edc41ff9e134850a20859a3ae073cadcce
EvgeniyKorshov <80601543+EvgeniyKorshov@users.noreply.github.com>
2023-02-03 14:23:39 +0300

    Update StatsResource.php

--- server/app/Http/Resources/StatsResource.php
+++ server/app/Http/Resources/StatsResource.php
@@ -15,12 +15,16 @@ class StatsResource extends JsonResource
     public function toArray($request)
     {
         return [
+
             'id' => $this->id,
             'player_id' => $this->player_id,
             'task_id' => $this->task_id,
             'time' => $this->time,
             'typed_characters' => $this->typed_characters,
             'number_of_errors' => $this->number_of_errors,
+            'Accuracy' => $this->Accuracy,
+            'dial_speeds' => $this->dial_speeds,
+            'created_at' => $this->created_at,
         ];
     }
 }
