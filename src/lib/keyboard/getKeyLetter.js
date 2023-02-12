a96dfac11d05cf2efb0cb8460f41e08647228c40
Shamil <toshamilgis@gmail.com>
2023-02-12 19:42:37 +0300

    fingers-update

---
+++ src/lib/keyboard/getKeyLetter.js
@@ -0,0 +1,26 @@
+export const getKeyLetter = language => {
+  switch (language) {
+    case "Русский":
+      return [
+        "ё12йфя",
+        "3цыч",
+        "4увс",
+        "56кеапми",
+        "0-=зхъжэ.\\",
+        "9щдю",
+        "8шлб",
+        "7нгроть",
+      ]
+    case "English":
+      return [
+        "§12qaz",
+        "3wsx",
+        "4edc",
+        "56rtfgvb",
+        "0-=p[];'/",
+        "9ol.",
+        "8ik,",
+        "7yuhjnm",
+      ]
+  }
+}
